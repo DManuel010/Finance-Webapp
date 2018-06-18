b38a6ceb2dbef4dbc70b7a7a0e2839ceff209fe2
Drew Manuel <manueld@wit.edu>
2018-06-18 16:40:30 -0400

    Initial commit

---
+++ src/main/java/com/cad/finance/FinanceWebappApplication.java
@@ -0,0 +1,12 @@
+package com.cad.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceWebappApplication.class, args);
+	}
+}
